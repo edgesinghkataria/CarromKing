5a88ab9e10f27b6a2bdea02df718fe2f16cc6697
ayushigupta931 <ayushigupta931@gmail.com>
2021-09-27 21:17:46 +0530

    Action Bar

--- app/src/main/java/com/android/carromking/activity/EnterOTPActivity.java
+++ app/src/main/java/com/android/carromking/activity/EnterOTPActivity.java
@@ -49,7 +49,7 @@ public class EnterOTPActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_otpactivity);
-
+        getSupportActionBar().hide();
         progressBar = new CustomProgressBar(this);
 
         TAG = getString(R.string.TAG);
