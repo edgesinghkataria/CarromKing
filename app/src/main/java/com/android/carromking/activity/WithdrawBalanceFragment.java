d2d8cbb34aaa913867b668322760aea2080cd008
Prakhar <prakhar@everything.net.in>
2021-10-29 16:01:23 +0530

    Fixed screen mixup

--- app/src/main/java/com/android/carromking/activity/WithdrawBalanceFragment.java
+++ app/src/main/java/com/android/carromking/activity/WithdrawBalanceFragment.java
@@ -74,7 +74,7 @@ public class WithdrawBalanceFragment extends Fragment {
         withdrawableBalance.setText( "₹ "+ localDataModel.getWinningBalance());
 
         withdrawNow.setOnClickListener(v -> requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                new WithdrawSuccessfulFragment()).commit());
+                new WithdrawSuccessfulFragment()).addToBackStack(null).commit());
 
         linkPaytm.setOnClickListener(v -> {
             LinkPaytm_Bottomsheet bottomSheet = new LinkPaytm_Bottomsheet();
