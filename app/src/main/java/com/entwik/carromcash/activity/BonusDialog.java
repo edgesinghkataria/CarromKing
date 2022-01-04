f64c1ab8551e092ae4b585ee8bba194b63666d22
puranjayK <puranjay01@gmail.com>
2022-01-04 15:59:51 +0530

    Added code for bonus dialog

--- app/src/main/java/com/entwik/carromcash/activity/BonusDialog.java
+++ app/src/main/java/com/entwik/carromcash/activity/BonusDialog.java
@@ -4,25 +4,46 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
+import com.entwik.carromcash.R;
 
 public class BonusDialog extends DialogFragment {
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        return new AlertDialog.Builder(getActivity())
-                .setTitle("Bonus")
-                .setMessage("Congrats! You got Rs. 10 signup bonus")
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.pop_up_money_reward,null);
+        dialogBuilder.setView(dialogView);
+        Button gotIt = (Button) dialogView.findViewById(R.id.btnGotIt);
+        AlertDialog alert = dialogBuilder.create();
+        gotIt.setOnClickListener(l->{
+            alert.dismiss();
+        });
 
-                    }
-                })
-                .create();
+        alert.show();
+
+        // Hide after some seconds
+        final Handler handler  = new Handler();
+        final Runnable runnable = () -> {
+            if (alert.isShowing()) {
+                alert.dismiss();
+            }
+        };
+
+        alert.setOnDismissListener(dialog -> handler.removeCallbacks(runnable));
+
+        handler.postDelayed(runnable, 5000);
+
+        return alert;
     }
 }
